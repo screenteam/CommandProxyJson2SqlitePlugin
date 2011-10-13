860eb15b8746de9b262c713f8f6e21302c366dc3
Martin Lenzelbauer <ml@screenteam.at>
2011-10-13 10:40:09 +0200

    initial commit

---
+++ src/at/screenteam/oma/JSON2SQLite.java
@@ -0,0 +1,88 @@
+package at.screenteam.oma;
+
+
+import commandproxy.core.Command;
+import commandproxy.core.CommandException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Reads the given JSON file using a stream parser and inserts the data into the given SQLite databse on the fly
+ * Used for the initial (full) OMa sync:
+ * - OMa downloads the complete database from the server, JSON encoded
+ * - OMa writes this JSON encoded string into a temporary file
+ * - OMa calls the DatabaseImporter to parse and import the data
+ * - DatabaseImporter processes the data and returns machine id, sync id and sync time
+ * - OMa deletes the temporary file and carries on
+ * 
+ * @author Martin Lenzelbauer <ml@screenteam.at>
+ */
+public class JSON2SQLite implements Command {
+
+	@Override
+	public String getName() {
+		return "json2sqlite";
+	}
+	
+	/**
+	 * imports the JSON data into the SQLite database
+	 * @param params needs two params: jsonFile (path to file with JSON data) and dbFile (path to SQLite database file)
+	 * @return JSON object containing the machine id, sync id and sync time read from the JSON input file
+	 * @throws CommandException
+	 * @throws JSONException 
+	 */
+	@Override
+	public JSONObject execute(Map<String, String> params) throws CommandException, JSONException {
+		String jsonFile = params.get("jsonFile");
+		String dbFile = params.get("dbFile");
+		
+		if (jsonFile == null || dbFile == null) {
+			throw new CommandException("Parameter jsonFile und/oder dbFile fehlt!", this);
+		}
+		
+		try {
+			// parse JSON and insert into SQLite database
+			FileReader reader = new FileReader(jsonFile);
+			JSONParser parser = new JSONParser();
+			JSONTransformer transformer = new JSONTransformer(dbFile);		
+
+			parser.parse(reader, transformer);
+
+			reader.close();
+			
+			// return machine id, sync id and sync time as JSON object
+			return new JSONObject(transformer.getReturnObject());	
+		}
+		catch (Exception e) {
+			throw new CommandException(e.getMessage(), this);
+		}
+	}
+
+	
+	
+	public static void main(String[] args) {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("jsonFile", "json.json");
+		params.put("dbFile", "db.db");
+		
+		JSON2SQLite plugin = new JSON2SQLite();
+		try {
+			JSONObject result = plugin.execute(params);
+			System.out.println("result: " + result);
+		} 
+		catch (CommandException ex) {
+			Logger.getLogger(JSON2SQLite.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (JSONException ex) {
+			Logger.getLogger(JSON2SQLite.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+	}
+	
+}
---
+++ src/at/screenteam/oma/JSONTransformer.java
@@ -0,0 +1,179 @@
+package at.screenteam.oma;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ContentHandler;
+import org.json.simple.parser.ParseException;
+
+public class JSONTransformer implements ContentHandler {
+
+	private JSONObject currObject;
+	private int level;
+	private String currKey;
+	private String currTable;	
+	private Set currTableFields;
+
+	private String dbPath;
+	private Connection conn;
+	private PreparedStatement stat;
+	
+	public JSONTransformer(String dbPath) {
+		this.dbPath = dbPath;
+	}
+	
+	//////////////////////////////////////////
+	// Interface ContentHandler
+	//////////////////////////////////////////
+	
+	@Override
+	public void startJSON() throws ParseException, IOException {
+		this.level = 0;
+		this.connect();
+	}
+	
+	@Override
+	public void endJSON() throws ParseException, IOException {
+		this.disconnect();
+	}
+
+	@Override
+	public boolean startObject() throws ParseException, IOException {
+		this.currObject = new JSONObject();
+		this.level++;
+		return true;
+	}
+	
+	@Override
+	public boolean endObject() throws ParseException, IOException {
+		this.level--;		
+		if (this.level == 1) {
+			this.endData();
+		}		
+		return true;
+	}
+
+	@Override
+	public boolean startObjectEntry(String key) throws ParseException, IOException {
+		this.currKey = key;
+		return true;
+	}
+	
+	@Override
+	public boolean endObjectEntry() throws ParseException, IOException {
+		return true;
+	}
+	
+	@Override
+	public boolean startArray() throws ParseException, IOException {
+		if (this.level == 1) {
+			this.currTable = this.currKey;
+			this.stat = null;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean endArray() throws ParseException, IOException {
+		if (this.level == 1) {
+			this.endTable();
+		}
+		return true;
+	}
+
+	@Override
+	public boolean primitive(Object value) throws ParseException, IOException {
+		this.currObject.put(this.currKey, value);
+		return true;
+	}
+	
+	//////////////////////////////////////////
+	// Additional methods
+	//////////////////////////////////////////
+
+	public JSONObject getReturnObject() {
+		JSONObject obj = new JSONObject();
+		obj.put("machineID", this.currObject.get("machineID"));
+		obj.put("syncID", this.currObject.get("syncID"));
+		obj.put("syncTime", this.currObject.get("syncTime"));
+		return obj;
+	}
+	
+	//////////////////////////////////////////
+	// Auxiliary methods
+	//////////////////////////////////////////
+
+	private void endTable() {
+		try {
+			this.stat.executeBatch();
+			this.conn.commit();
+		} 
+		catch (SQLException ex) {
+			Logger.getLogger(JSONTransformer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	private void endData() {
+		if (this.stat == null) {
+			this.prepareStatement();
+		}
+		
+		try {
+			int i = 1;
+			for (Object key: this.currTableFields) {
+				this.stat.setString(i, this.currObject.get(key).toString());
+				i++;
+			}
+			this.stat.addBatch();
+		} 
+		catch (SQLException ex) {
+			Logger.getLogger(JSONTransformer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	private void prepareStatement() {
+		try {
+			String fields = "";
+			String values = "";
+			this.currTableFields = this.currObject.keySet();
+			for (Object key: this.currTableFields) {
+				fields += ",`" + key + "`";
+				values += ",?";
+			}
+			fields = fields.substring(1);
+			values = values.substring(1);
+			this.stat = this.conn.prepareStatement("INSERT INTO " + this.currTable + "(" + fields + ") VALUES (" + values + ");");
+//			System.out.println("INSERT INTO " + this.currTable + "(" + fields + ") VALUES (" + values + ");");
+		} 
+		catch (SQLException ex) {
+			Logger.getLogger(JSONTransformer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	private void connect() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			this.conn = DriverManager.getConnection("jdbc:sqlite:" + this.dbPath);
+			this.conn.setAutoCommit(false);
+		} 
+		catch (Exception ex) {
+			Logger.getLogger(JSONTransformer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	private void disconnect() {
+		try {
+			this.conn.close();
+		} 
+		catch (Exception ex) {
+			Logger.getLogger(JSONTransformer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+}
